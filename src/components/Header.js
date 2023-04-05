e333cea78fed187d034eaa4a9a95003cc960e46d
d1 <www.didowu@gmail.com>
2023-04-05 13:28:44 +0100

    added header and navigation html

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,54 @@ function Header()  {
       <div>
         <Mobile></Mobile>
         <Navigation></Navigation>
+        <div id="header_wrapper_2">
+          <div class="">
+            <div id="header_shade_2">
+              <div id="header_shade"></div>
+            </div>
+          </div>
+          <div class="">
+            <div id="u_header_shade_2">
+              <div id="u_header_shade"></div>
+            </div>
+          </div>
+          <div id="header_wrapper_1">
+            <div id="header_wrapper">
+              <div id="header_extra_1"></div>
+              <div id="header_extra_2"></div>
+              <header role="banner" id="header_img_none">
+                <div id="inner_banner">
+                  <div id="header_extra_3"></div>
+                  <div id="header_extra_4"></div>
+
+                  <hgroup
+                    onclick="document.location='/';"
+                    style={{cursor: "pointer"}}
+                  >
+                    <div class="h1_outer outer_title">
+                      <div class="h1_bg"></div>
+                      <h1 class="site_hdr">
+                        Canada International Conference On Engineering and
+                        Sciences
+                      </h1>
+                    </div>
+                    <div class="header_spacer"></div>
+                    <div class="h2_outer">
+                      <div class="h2_bg"></div>
+                      <h2 class="site_hdr">
+                        CICES 2023 |May 21 -25, 2023 | Calgary, Canada
+                      </h2>
+                    </div>
+                  </hgroup>
+                </div>
+                {/* <!-- END OF 'inner_banner' --> */}
+              </header>
+              {/* <!-- END OF 'header' --> */}
+            </div>
+            {/* <!-- END OF 'header_wrapper' --> */}
+          </div>
+          {/* <!-- END OF 'header_wrapper_1' --> */}
+        </div>
       </div>
     );
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,138 @@
 function Navigation() {
     return (
-      <div >
-        
+      <div id="navigation_wrapper">
+        <div id="nav_border_top"></div>
+        <div id="menus_x1"></div>
+        <div id="menus_x2"></div>
+        <div id="navigation">
+          <nav id="h_nav">
+            <ul id="main_menu_h">
+              <li
+                style={{zIndex: "1"}}
+                id="menu14686303"
+                class="menu_top h_menu_selected"
+              >
+                <span class="main_menu_outer_2_selected">
+                  <span class="main_menu_outer_1_selected">
+                    <span class="outer_menu_x">
+                      <span class="menu_x1"></span>
+                      <span class="menu_x2"></span>
+                      <a
+                        class="outer_title menu main_menu_selected"
+                        href="home.html"
+                      >
+                        <span class="menu_wrap">Home</span>
+                      </a>
+                    </span>
+                  </span>
+                </span>{" "}
+              </li>
+              <li style={{zIndex: "1"}} id="menu14686302" class="menu_top h_menu">
+                <span class="main_menu_outer_2">
+                  <span class="main_menu_outer_1">
+                    <span class="outer_menu_x">
+                      <span class="menu_x1"></span>
+                      <span class="menu_x2"></span>
+                      <a
+                        class="outer_title menu main_menu"
+                        href="papersubmission.html"
+                      >
+                        <span class="menu_wrap">Call For Paper</span>
+                      </a>
+                    </span>
+                  </span>
+                </span>{" "}
+              </li>
+              <li style={{zIndex: "1"}} id="menu14686304" class="menu_top h_menu">
+                <span class="main_menu_outer_2">
+                  <span class="main_menu_outer_1">
+                    <span class="outer_menu_x">
+                      <span class="menu_x1"></span>
+                      <span class="menu_x2"></span>
+                      <a
+                        class="outer_title menu main_menu"
+                        href="registration.html"
+                      >
+                        <span class="menu_wrap">Registration</span>
+                      </a>
+                    </span>
+                  </span>
+                </span>{" "}
+              </li>
+              <li style={{zIndex: "1"}} id="menu14874771" class="menu_top h_menu">
+                <span class="main_menu_outer_2">
+                  <span class="main_menu_outer_1">
+                    <span class="outer_menu_x">
+                      <span class="menu_x1"></span>
+                      <span class="menu_x2"></span>
+                      <a
+                        class="outer_title menu main_menu"
+                        href="travel-and-hotel.html"
+                      >
+                        <span class="menu_wrap">Travel and Hotel</span>
+                      </a>
+                    </span>
+                  </span>
+                </span>{" "}
+              </li>
+              <li
+                style={{zIndex: "1"}}
+                id="menu14874784"
+                class="menu_top h_menu align_right"
+              >
+                <span class="main_menu_outer_2">
+                  <span class="main_menu_outer_1">
+                    <span class="outer_menu_x">
+                      <span class="menu_x1"></span>
+                      <span class="menu_x2"></span>
+                      <a
+                        class="outer_title menu main_menu"
+                        href="speaker-and-committee.html"
+                      >
+                        <span class="menu_wrap">Speaker and Committee</span>
+                      </a>
+                    </span>
+                  </span>
+                </span>{" "}
+              </li>
+              <li
+                style={{zIndex: "1"}}
+                id="menu14874799"
+                class="menu_top h_menu align_right"
+              >
+                <span class="main_menu_outer_2">
+                  <span class="main_menu_outer_1">
+                    <span class="outer_menu_x">
+                      <span class="menu_x1"></span>
+                      <span class="menu_x2"></span>
+                      <a
+                        class="outer_title menu main_menu"
+                        href="canada-visa.html"
+                      >
+                        <span class="menu_wrap">Canada Visa</span>
+                      </a>
+                    </span>
+                  </span>
+                </span>{" "}
+              </li>
+              <li style={{zIndex: "1"}} id="menu14686305" class="menu_top h_menu">
+                <span class="main_menu_outer_2">
+                  <span class="main_menu_outer_1">
+                    <span class="outer_menu_x">
+                      <span class="menu_x1"></span>
+                      <span class="menu_x2"></span>
+                      <a class="outer_title menu main_menu" href="contact.html">
+                        <span class="menu_wrap">contact Us</span>
+                      </a>
+                    </span>
+                  </span>
+                </span>{" "}
+              </li>
+            </ul>
+          </nav>
+        </div>
+        {/* <!-- END OF 'navigation' --> */}
+        <div id="nav_border_bottom"></div>
       </div>
     );
   }
