cf2bcbdf8a7535d9c4306def73882414a612f31b
d1 <www.didowu@gmail.com>
2023-04-05 16:49:46 +0100

    added contact ,speaker,visa and travel html

--- src/pages/Speaker.js
+++ src/pages/Speaker.js
@@ -1,8 +1,239 @@
+import Side from "../components/Side";
 
 function Speaker() {
   return (
-    <div className="inner_body">
-      
+    <div>
+      <Side />
+      <div id="content" class=" page_header_fade_0">
+        <div id="content_inner_1">
+          <div id="main_content">
+            <div id="main_content_bg"></div>
+            <section id="quick_edit_container">
+              <div id="quick_content">
+                <div id="content_headers">
+                  <div id="content_headers_inner_1">
+                    <div class="hgroup_outer">
+                      <hgroup id="page_titles">
+                        <div class="outer_title">
+                          <h1>Speaker and Committee</h1>
+                        </div>
+                        <span class="sub_title_spacer"></span>
+                        <h2>Featured Speakers</h2>
+                      </hgroup>
+                    </div>
+                  </div>
+                  <div id="content_headers_bg_outer">
+                    <div id="content_headers_bg_fade"></div>
+                    <div id="content_headers_bg_outer_1">
+                      <div id="content_headers_bg"></div>
+                      <div id="content_headers_bg2"></div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="content_body">
+                  <div class="content_body_inner_1">
+                    <div class="content_body_inner_2">
+                      <h2>
+                        <img
+                          src="./speaker-and-committee_files/kawasaki.jpg"
+                          style={{marginBottom: "0px", borderColor: "rgb(236, 226, 206)", fontSize: "14px", backgroundPosition: "0px 0px", color: "rgb(69, 49, 34)", boxSizing: "border-box",textSizeAdjust: "100%", position: "relative", width: "176px"}}
+                        />{" "}
+                        <img
+                          src="./speaker-and-committee_files/041d18b.jpg"
+                          style={{borderColor: "rgb(236, 226, 206)",fontSize: "12px", color: "rgb(69, 49, 34)", fontFamily: "Arial, Helvetica, sans-serif", width: "205px"}}
+                        />
+                      </h2>
+                      <h3>
+                        <p>
+                          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                          &nbsp; &nbsp; &nbsp; &nbsp; Thomas Wuerthinger
+                          <br />
+                          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                          &nbsp; &nbsp; &nbsp; &nbsp;University Of
+                          Calgary&nbsp;&nbsp;
+                          <br />
+                          <span ></span>
+                          <span >
+                            Thomas Wuerthinger is a Senior Research Director at
+                            Oracle Labs leading programming language
+                            implementation teams for languages including Java,
+                            JavaScript, Ruby, and R. He is the architect of the
+                            Graal compiler and the Truffle self-optimizing
+                            runtime system.
+                          </span>
+                        </p>
+                      </h3>
+                      <h2>
+                        <p>
+                          <img src="./speaker-and-committee_files/ac.jpg" />
+                        </p>
+                      </h2>
+                      <h3>Andrew Vaughn</h3>
+                      <h2>
+                        <p>University Of Calgary</p>
+                        <p>
+                          <img src="./speaker-and-committee_files/add.jpg" />
+                        </p>
+                      </h2>
+                      <h3>Hamilton Lohan</h3>
+                      <h2>
+                        <p>
+                          University of Miami,
+                          <br />
+                          USA
+                        </p>
+                        <div>
+                          <p>
+                            <img
+                              src="./speaker-and-committee_files/ty.jpg"
+                              style={{height: "auto", width: "273px"}}
+                            />
+                          </p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                        </div>
+                      </h2>
+                      <h3>Anne Bachmann</h3>
+                      <h2>
+                        <p></p>
+                        <div>
+                          <p>University Of Calgary</p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                        </div>
+                      </h2>
+                      <h2>Keynote Speaker</h2>
+                      <h2>
+                        <p></p>
+                        <div>
+                          <br />
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                        </div>
+                      </h2>
+                      <h3>Naresh Agarwal</h3>
+                      <h2>
+                        <p></p>
+                        <div>
+                          <div>
+                            <div>
+                              <div>
+                                <div>
+                                  <div>
+                                    <div>
+                                      <p>
+                                        Simmons University,
+                                        <br />
+                                        USA
+                                      </p>
+                                      <p></p>
+                                      <p></p>
+                                      <p></p>
+                                      <p></p>
+                                      <p></p>
+                                      <p></p>
+                                      <p></p>
+                                    </div>
+                                    <p></p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </h2>
+                      <h4>Education</h4>
+                      <h2>
+                        <p></p>
+                        <div>
+                          <div>
+                            <div>
+                              <div>
+                                <div>
+                                  <div>
+                                    <div>
+                                      <div>
+                                        <div>
+                                          <p>
+                                            Ph.D.
+                                            <br />
+                                            National University of Singapore,
+                                            Singapore
+                                          </p>
+                                          <p>
+                                            B.A.Sc.
+                                            <br />
+                                            Nanyang Technological University,
+                                            Singapore
+                                          </p>
+                                          <p></p>
+                                          <p></p>
+                                          <p></p>
+                                          <p></p>
+                                          <p></p>
+                                          <p></p>
+                                          <p></p>
+                                        </div>
+                                        <p></p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </h2>
+                      <h4>Research Areas</h4>
+                      <h2>
+                        <p></p>
+                        <div>
+                          <div>
+                            <p>
+                              Information Behavior
+                              <br />
+                              Knowledge Management
+                            </p>
+                            <p>
+                              <br />
+                            </p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                          </div>
+                          <p></p>
+                        </div>
+                      </h2>
+                    </div>
+                  </div>
+                  <div class="content_body_bg"></div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Travel.js
+++ src/pages/Travel.js
@@ -1,8 +1,160 @@
+import Side from "../components/Side";
 
 function Travel() {
   return (
-    <div className="inner_body">
-      
+    <div>
+      <Side />
+      <div id="content" class=" page_header_fade_0">
+        <div id="content_inner_1">
+          <div id="main_content">
+            <div id="main_content_bg"></div>
+            <section id="quick_edit_container">
+              <div id="quick_content">
+                <div id="content_headers">
+                  <div id="content_headers_inner_1">
+                    <div class="hgroup_outer">
+                      <hgroup id="page_titles">
+                        <div class="outer_title">
+                          <h1>Travel and Hotel</h1>
+                        </div>
+                      </hgroup>
+                    </div>
+                  </div>
+                  <div id="content_headers_bg_outer">
+                    <div id="content_headers_bg_fade"></div>
+                    <div id="content_headers_bg_outer_1">
+                      <div id="content_headers_bg"></div>
+                      <div id="content_headers_bg2"></div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="content_body">
+                  <div class="content_body_inner_1">
+                    <div class="content_body_inner_2">
+                      <p>
+                        The CICES 2023 will take place in an around the Harley
+                        Davidson,&nbsp;2478 Pegasus Rd NE, Calgary, AB T2E 8G8,
+                        Canada.
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                      <p>
+                        Calgary International Airport, branded as YYC Calgary
+                        International Airport, is an international airport that
+                        serves the city of Calgary, Alberta, Canada. It is
+                        located approximately 17 km northeast of downtown and
+                        covers an area of 21.36 km².
+                      </p>
+                      <p>
+                        <img
+                          src="./travel-and-hotel_files/images.jpg"
+                          style={{borderColor: "rgb(236, 226, 206)", fontSize: "15px"}}
+                        />
+                      </p>
+                      <p>
+                        The Calgary International Airport is the closest airport
+                        to the conference venue.
+                        <br />
+                        CALGARY AIRPORT SHUTTLE SERVICE
+                      </p>
+                      <p>
+                        Allied Airport Shuttle has the largest fleet of vehicles
+                        in the greater Calgary and surrounding area to meet any
+                        transportation need. They have over 40 years of
+                        experience providing quality service to the public,
+                        business community, schools and sports teams with safe,
+                        clean and professionally-driven vehicles.
+                      </p>
+                      <p>
+                        Allied Airport Shuttle provides affordable, regularly
+                        scheduled transportation, in fully automated GPS
+                        dispatched shuttle buses, between designated points
+                        within the City of Calgary and the Calgary International
+                        Airport.
+                      </p>
+                      <p>
+                        <img
+                          src="./travel-and-hotel_files/Transit_1.png"
+                          style={{borderColor: "rgb(236, 226, 206)",fontSize: "15px"}}
+                        />
+                      </p>
+                      <p>
+                        Delegates who notify the organizers of their itinerary
+                        not later than May 10, 2023 can benefit from the
+                        international delegate airport pickup from the Calgary
+                        International Airport to designated hotels booked
+                        through the organizers.
+                      </p>
+                      <p>
+                        <strong>Accommodation</strong>
+                      </p>
+                      <p>
+                        We encourage you to book your accommodation as early as
+                        possible. The conference dates are within the high peak
+                        season and therefore rooms will be limited unless you
+                        book early. We recommend booking your accommodation
+                        before&nbsp;May 10, 2023
+                      </p>
+                      <p>
+                        <strong>Options for accommodation</strong>
+                      </p>
+                      <p>
+                        Options for low-cost accommodation for the dates of the
+                        conference are limited. If you require accommodation
+                        please contact the organisers
+                      </p>
+                      <p>
+                        There are a number of hotels in close proximity to the
+                        event venue
+                      </p>
+                      <p>
+                        Delta Bow Valley - 209-4th Avenue Southeast Calgary, AB
+                        (403) 266-1980
+                      </p>
+                      <p>
+                        International Hotel Suites - 220 4 Avenue Southwest
+                        Calgary, AB (403) 265-9600
+                      </p>
+                      <p>
+                        The Westin Calgary - 320 4 Avenue Southwest Calgary, AB
+                        (403) 266-1611
+                      </p>
+                      <p>
+                        Sheraton Suites Calgary - 255 Barclay Parade Southwest
+                        Calgary, AB (403) 266-7200
+                      </p>
+                      <p>
+                        Regency Suites Calgary - 610 4th Avenue SW Calgary, AB
+                        T2P 0K1 (403) 231-1000
+                      </p>
+                      <p>
+                        Sandman Hotel Downtown - 888 7 Avenue Southwest, Calgary
+                        (403) 237-8626
+                      </p>
+                      <p>
+                        Holiday Inn Express Downtown - 1020 8th Ave SW, Calgary
+                        (403) 269-8262
+                      </p>
+                      <p>
+                        Best Western Downtown - 1330 8th St SW Calgary, AB (403)
+                        228-6900
+                      </p>
+                      <p>Nuvo Hotel - 827 12 Ave SW Calgary, AB</p>
+                      <p>
+                        Hilton Garden Inn Calgary Downtown - 711 4th St. S.E.,
+                        Calgary, AB, (587) 352-2020
+                      </p>
+                    </div>
+                  </div>
+                  <div class="content_body_bg"></div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
