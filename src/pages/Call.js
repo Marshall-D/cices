7f2582f22076c76dbcf2bdcee2c94c81bde27371
d1 <www.didowu@gmail.com>
2023-04-05 16:15:56 +0100

    added call html

--- src/pages/Call.js
+++ src/pages/Call.js
@@ -1,9 +1,170 @@
+import Side from "../components/Side";
 
 function Call() {
   return (
-    <div className="inner_body">
-      
+    <div>
+      <Side/>
+    <div id="content" class=" page_header_fade_0">
+      <div id="content_inner_1">
+        <div id="main_content">
+          <div id="main_content_bg"></div>
+          <section id="quick_edit_container">
+            <div id="content_headers">
+              <div id="content_headers_inner_1">
+                <div class="hgroup_outer">
+                  <hgroup id="page_titles">
+                    <div class="outer_title">
+                      <h1>Paper Submission</h1>
+                    </div>
+                  </hgroup>
+                </div>
+              </div>
+              <div id="content_headers_bg_outer">
+                <div id="content_headers_bg_fade"></div>
+                <div id="content_headers_bg_outer_1">
+                  <div id="content_headers_bg"></div>
+                  <div id="content_headers_bg2"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="content_body">
+              <div class="content_body_inner_1">
+                <div class="content_body_inner_2">
+                  <p>
+                    The Canada International Conference On Engineering and
+                    Sciences&nbsp; (CICES 2023) offers a great opportunity to
+                    bring together professors, researchers, and scholars around
+                    the globe, having a great platform to deliver the latest
+                    innovative research result and the most recent development
+                    and trends in engineering and natural sciences field.
+                  </p>
+                  <p>
+                    2023 CICES will take place from May 21-25, 2023 in Calgary,
+                    Canada. The scholars are encouraged to submit full papers or
+                    abstracts on any aspect of engineering and natural science.
+                    All manuscripts submissions should be sent via email -
+                    submission (at) blader .com
+                  </p>
+                  <p>TOPICS of interested includes, but not limited to</p>
+                  <p>- Biological Engineering</p>
+                  <p>- Chemical Engineering</p>
+                  <p>- Civil Engineering</p>
+                  <p>- Computer and Information Sciences</p>
+                  <p>- Electrical and Electronic Engineering</p>
+                  <p>- Environmental Science</p>
+                  <p>- Fundamentals Of Natural Sciences</p>
+                  <p>- Power and Energy Engineering</p>
+                  <p>- Material Science and Engineering</p>
+                  <p>- Mechanical Engineering</p>
+                  <p>
+                    Please submit an abstract to: submission (at) blader.com,
+                    not later than May 5, 2023&nbsp; to: Ass.Prof. Martha Blake.
+                    Please include: Title of Paper, Full Name (s), Current
+                    Position, Institutional Affiliation, an email address and at
+                    least 3 keywords that best describe the subject of your
+                    submission.{" "}
+                  </p>
+                  <p>
+                    If your submission is accepted, you should be notified
+                    within 48 hrs, issued an author number and required to
+                    register online for the conference. Should you wish to
+                    participate in the Conference without presenting a paper,
+                    for example, to chair a session, to evaluate papers which
+                    are to be included in the conference proceedings or books,
+                    to contribute to the editing of a book, or any other
+                    contribution, please send an email to Dr. Felicity Lohan,
+                    felicity (at) blader.com.
+                  </p>
+                  <p>
+                    <strong>Abstract Submission Guidelines</strong>
+                  </p>
+                  <p>
+                    Organizers of the conference welcome submissions written in
+                    English language ONLY that are closely related to the areas
+                    specified in the call for papers.
+                  </p>
+                  <p>
+                    Abstract should not exceed 300 words and it must clearly
+                    indicate the problem, objectives, research methods, results
+                    and conclusions.
+                  </p>
+                  <p>
+                    Submitted abstracts must fit online format that doesn't
+                    register long abstracts and abstracts that include graphics
+                    and tables.
+                  </p>
+                  <p>
+                    Authors are also advised to proofread their abstracts
+                    because abstracts will be reproduced from the online
+                    submission. Any error in spelling, grammar or data will
+                    appear online or in print.
+                  </p>
+                  <p>
+                    Authors of the submitted abstracts for presentation must
+                    also make separate registration for the conference by
+                    filling in the Registration Form.
+                  </p>
+                  <p>
+                    Submission of abstracts doesn't include official
+                    registration for the conference. If the abstract is
+                    submitted by two authors, both of them must make the
+                    official registration.
+                  </p>
+                  <p>
+                    <strong>Preparation of Poster Presentation</strong>
+                  </p>
+                  <p>
+                    Please make sure that you have prepared the Poster
+                    Presentation according to the Template of Poster
+                    Presentation below.
+                  </p>
+                  <p>
+                    <a
+                      href="https://icens2019.doodlekit.com/file/353155/ICENS_poster_templete.pptx"
+                      style={{fontSize: "14px"}}
+                    >
+                      Click here to see
+                    </a>{" "}
+                    Template of Poster Presentation (Poster size is 60cm*84cm)
+                  </p>
+                  <p>
+                    <img
+                      src="./papersubmission_files/g.jpg"
+                      style={{height: "auto", width: "557"}}
+                    />
+                  </p>
+                  <p>
+                    <img
+                      src="./papersubmission_files/s.jpeg"
+                      style={{height: "auto", width: "536"}}
+                    />
+                  </p>
+                </div>
+              </div>
+              <div class="content_body_bg"></div>
+            </div>
+
+            {/* 
+<script type="text/javascript">(function() {
+$(function() {
+$(document).on('ajax:beforeSend', 'a.delete_album', function(e) {
+return $(this).closest('.pic').fadeOut();
+});
+return $(document).on('ajax:success', '.up a, .down a', function(e) {
+return $('#main_content').load('/papersubmission.html');
+});
+});
+
+}).call(this);
+</script> */}
+          </section>
+        </div>
+      </div>
     </div>
+
+    </div>
+    
   );
 }
 
