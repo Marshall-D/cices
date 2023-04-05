6e81964f373dcfd477aaf71f43ceca662512379a
d1 <www.didowu@gmail.com>
2023-04-05 16:23:24 +0100

    added registration html

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,8 +1,285 @@
+import Side from "../components/Side";
 
 function Registration() {
   return (
-    <div className="inner_body">
-      
+    <div>
+      <Side />
+      <div id="content" class=" page_header_fade_0">
+        <div id="content_inner_1">
+          <div id="main_content">
+            <div id="main_content_bg"></div>
+            <section id="quick_edit_container">
+              <div id="quick_content">
+                <div id="content_headers">
+                  <div id="content_headers_inner_1">
+                    <div class="hgroup_outer">
+                      <hgroup id="page_titles">
+                        <div class="outer_title">
+                          <h1>Registration</h1>
+                        </div>
+                      </hgroup>
+                    </div>
+                  </div>
+                  <div id="content_headers_bg_outer">
+                    <div id="content_headers_bg_fade"></div>
+                    <div id="content_headers_bg_outer_1">
+                      <div id="content_headers_bg"></div>
+                      <div id="content_headers_bg2"></div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="content_body">
+                  <div class="content_body_inner_1">
+                    <div class="content_body_inner_2">
+                      <div id="content_message">
+                        <div class="content_message">
+                          <table>
+                            <tbody>
+                              <tr>
+                                <td>
+                                  <strong></strong>
+                                </td>
+                                <td>
+                                  <strong>&nbsp; &nbsp;Early bird</strong>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                  &nbsp;&nbsp;
+                                </td>
+                                <td>
+                                  <strong>
+                                    Until May&nbsp;2&nbsp; &nbsp; &nbsp; &nbsp;
+                                    &nbsp; &nbsp; &nbsp;
+                                  </strong>
+                                  <br />
+                                  <strong>
+                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                    &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;
+                                  </strong>
+                                </td>
+                                <td>
+                                  <strong>
+                                    {" "}
+                                    Until May 2<br />
+                                    <br />
+                                  </strong>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Author </strong>
+                                </td>
+                                <td>
+                                  <strong>700 USD</strong>
+                                </td>
+                                <td>
+                                  <strong>800 USD</strong>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Listener</strong>
+                                </td>
+                                <td>
+                                  <strong>500 USD</strong>
+                                </td>
+                                <td>
+                                  <strong>600 USD</strong>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Student</strong>
+                                </td>
+                                <td>
+                                  <strong>400 USD</strong>
+                                </td>
+                                <td>
+                                  <strong>400 USD</strong>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>
+                                    Group Registration&nbsp; &nbsp; &nbsp;
+                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                    &nbsp;
+                                  </strong>
+                                </td>
+                                <td>
+                                  <strong>400 USD</strong>
+                                </td>
+                                <td>
+                                  <strong>400 USD</strong>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                          <p>The Registration Fee includes:</p>
+                          <table>
+                            <tbody>
+                              <tr>
+                                <td>For Authors</td>
+                                <td>For Listeners and Students</td>
+                              </tr>
+                              <tr>
+                                <td>Paper presentation</td>
+                              </tr>
+                              <tr>
+                                <td>Proceedings (PDF in USB) </td>
+                                <td>Proceedings (PDF in USB)</td>
+                              </tr>
+                              <tr>
+                                <td>Attendance to all sessions</td>
+                                <td>Attendance to all sessions</td>
+                              </tr>
+                              <tr>
+                                <td>Conference program</td>
+                                <td>Conference program</td>
+                              </tr>
+                              <tr>
+                                <td>Name badge</td>
+                                <td>Name badge</td>
+                              </tr>
+                              <tr>
+                                <td>Certificate of Presentation</td>
+                                <td>Certificate of attendance</td>
+                              </tr>
+                              <tr>
+                                <td>Conference bag</td>
+                                <td>Conference bag</td>
+                              </tr>
+                              <tr>
+                                <td>Lunches and coffee breaks</td>
+                                <td>Lunches and coffee breaks</td>
+                              </tr>
+                              <tr>
+                                <td>Paper Publication</td>
+                              </tr>
+                              <tr>
+                                <td>Author Number</td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>
+                                    Refund and Cancellation Policy
+                                  </strong>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>In case of visa refusal, delegates are</td>
+                                <td>eligible for a full refund </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  of the registration fee, excluding transfer
+                                </td>
+                                <td>charges</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                          <p>
+                            <a
+                              href="https://form.jotform.com/222753850645460"
+                              target="_blank"
+                            >
+                              <br />
+                              <img
+                                src="./registration_files/Button_Small_Biz_Conference_Register_Now.png"
+                                alt=""
+                                style={{borderColor: "rgb(236, 226, 206)", fontSize: "15px", width: "290px"}}
+                              />
+                            </a>
+                          </p>
+                          <p>
+                            <strong>Certificate</strong>
+                          </p>
+                          <p>
+                            The organizers will issue only ONE certificate for
+                            each submission that complete the registration. For
+                            example, 2 authors of one submission all complete
+                            the payment, they will receive 2 certificate of
+                            presentation. If only 1 author complete the payment,
+                            he/she will be issued one certificate of
+                            presentation and other co-author won't be issued the
+                            certificate.
+                          </p>
+                          <p>
+                            Certificate of Presentation --&gt; for authors
+                            (indicates a presenter’s name, affiliation and the
+                            paper title that is presented in the scheduled
+                            session)
+                          </p>
+                          <p>
+                            Certificate of Attendance--&gt; for Listeners
+                            (includes participant’s name and affiliation,
+                            certifying the participation in the conference)
+                          </p>
+                          <p>
+                            <strong>Payment</strong>
+                          </p>
+                          <p>
+                            The CICES 2023 registration fee is paid according to
+                            your location.{" "}
+                          </p>
+                          <p>Bank Transfer (TT)</p>
+                          <p>Bitcoin</p>
+                          <p>
+                            Instant Money Transfer (Western Union or MoneyGram)
+                          </p>
+                          <p>PayPal</p>
+                          <p>
+                            Before making the payment for the conference using
+                            any of the above methods, please register online and
+                            select your registration fee category and
+                            registration fee.
+                          </p>
+                          <p>
+                            Delegates from <strong>India </strong>may pay the
+                            registration fee using{" "}
+                            <strong>Local Indian currency INR</strong> at a
+                            designated bank in India.
+                          </p>
+                          <p>
+                            Delegates from other
+                            <strong> Africa, Middle East , Europe</strong> and{" "}
+                            <strong>Asia</strong> may pay the registration fee
+                            using Western Union, Money Gram
+                          </p>
+                          <p>and Bank Wire Transfer.</p>
+                          <p>
+                            Delegates from US and Canada may pay directly to our
+                            Scotia Bank Account, Account Branch - Calgary,
+                            Canada.
+                          </p>
+                          <p>
+                            Cash on site or personal checks will not be
+                            accepted.
+                          </p>
+                          <p>
+                            For those paying in local currency, the exchange
+                            rate will be subject to the current exchange rate at
+                            the time of your registration.
+                          </p>
+                        </div>
+                        <div class="content_headers" id="hr_div_sep"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="content_body_bg"></div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
