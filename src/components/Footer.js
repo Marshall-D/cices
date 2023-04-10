1c8e20da8805fe54432937de950e18746c4a53ce
d1 <www.didowu@gmail.com>
2023-04-10 16:36:25 +0100

    added footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,29 @@
+
+import "../Styles/Footer.css";
+
 function Footer() {
     return (
-      <div id="footer_wrapper_3">
-      <div id="footer_wrapper_2">
-         <div class="">
-            <div id="footer_shade_2">
-               <div id="footer_shade"></div>
-            </div>
-         </div>
-         <div id="footer_wrapper_1">
-            <div id="footer_wrapper">
-               <footer role="contentinfo">
-                  <div id="custom_footer"><p>Copyright © 2019-2023. All Rights Reserved.</p></div>
+      // <div id="footer_wrapper_3">
+      // <div id="footer_wrapper_2">
+      //    <div class="">
+      //       <div id="footer_shade_2">
+      //          <div id="footer_shade"></div>
+      //       </div>
+      //    </div>
+         <div className="footer_wrapper_1">
+            {/* <div id="footer_wrapper"> */}
+               <footer role="contentinfo"
+               className="content_info" >
+                  <div className="custom_footer"><p>Copyright © 2019-2023. All Rights Reserved.</p></div>
                   <div style={{clear:"both"}}></div>
                     
                </footer>
                {/* <!-- END OF 'footer' --> */}
-            </div>
+            {/* </div> */}
             {/* <!-- END OF 'footer_wrapper' --> */}
-         </div>
+         {/* </div> */}
          {/* <!-- END OF 'footer_wrapper_1' --> */}
-      </div>
+      {/* </div> */}
       {/* <!-- END OF 'footer_wrapper_2' --> */}
    </div>
     );
