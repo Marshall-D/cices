1a239e5510525980fa8ee0e853c6861ed5b3b442
d1 <www.didowu@gmail.com>
2023-04-10 14:07:42 +0100

    nav links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,8 +6,16 @@ import "../Styles/colinNav.css";
 function Navigation() {
     return (
       <nav className="nav">
-        <span className="span">contact us</span>
-        <span className="span">contact us</span>
+        <span className='span'>
+    
+          <Link to="/" className="span">
+            Home
+          </Link>
+          
+        </span>
+        <span className="span"> <Link to="/reg"  className="span">
+            Home
+          </Link></span>
         <span className="span">contact us</span>
         <span className="span">contact us</span>
         <span className="span">contact us</span>
