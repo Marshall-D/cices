8a5566908f110fb95a6fb988c5c032d37ac67196
d1 <www.didowu@gmail.com>
2025-01-04 18:44:54 +0100

    change filtering to use username

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -25,7 +25,7 @@ const HomePage: React.FC = () => {
   }, []);
 
   const filteredUsers = users.filter((user) =>
-    user.name.toLowerCase().includes(searchQuery.toLowerCase())
+    user.username.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
